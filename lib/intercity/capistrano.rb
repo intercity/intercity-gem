d785c7895d559e1682e7a4db62ef57bff0ac1d30
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-06-23 18:20:37 +0200

    Added bluepill restarting resources

--- lib/intercity/capistrano.rb
+++ lib/intercity/capistrano.rb
@@ -1,5 +1,10 @@
 Capistrano::Configuration.instance(:must_exist).load do
 
+  set :user, "deploy"
+  set :use_sudo, false
+  default_run_options[:pty] = true
+  ssh_options[:forward_agent] = true
+
   after "deploy:finalize_update", "db:symlink"
   after "deploy:finalize_update", "unicorn:symlink"
 
@@ -12,6 +17,21 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :unicorn do
     task :symlink do
       run "ln -nfs #{shared_path}/sockets #{release_path}/tmp/sockets"
+    end
+  end
+
+  namespace :deploy do
+    task :start do
+      run "sudo bluepill load /etc/bluepill/#{application}.pill"
+    end
+    task :stop do
+      run "sudo bluepill #{application} stop"
+    end
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      run "sudo bluepill #{application} restart"
+    end
+    task :status do
+      run "sudo bluepill #{application} status"
     end
   end
 
